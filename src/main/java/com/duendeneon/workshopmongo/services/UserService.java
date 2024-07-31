c36b98773bde5cefd4b05eb05547e366db01521e
Arthur Henrique <duendeneon@hotmail.com>
2024-07-31 18:41:39 -0300

    Connecting to MongoDB with repository and service

---
+++ src/main/java/com/duendeneon/workshopmongo/services/UserService.java
@@ -0,0 +1,22 @@
+package com.duendeneon.workshopmongo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.duendeneon.workshopmongo.domain.User;
+import com.duendeneon.workshopmongo.repository.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repo;
+	
+	public List<User> findAll(){
+		return repo.findAll();
+	}
+	
+
+}
