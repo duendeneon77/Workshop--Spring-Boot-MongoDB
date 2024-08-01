18477927304b06e9ed0bc0bfcdb71e5b8907ef50
Arthur Henrique <duendeneon@hotmail.com>
2024-08-01 04:14:16 -0300

    Getting the user by id

---
+++ src/main/java/com/duendeneon/workshopmongo/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,17 @@
+package com.duendeneon.workshopmongo.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+	
+	public ObjectNotFoundException(String msg) {
+		super(msg);
+		
+	}
+	
+	
+	
+	
+	
+
+}
