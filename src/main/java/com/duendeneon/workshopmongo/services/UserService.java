13b6b69806ef207aa0c529900f7a1ee872953432
Arthur Henrique <duendeneon@hotmail.com>
2024-08-02 03:06:25 -0300

    User insertion with using POST

--- src/main/java/com/duendeneon/workshopmongo/services/UserService.java
+++ src/main/java/com/duendeneon/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.duendeneon.workshopmongo.domain.User;
+import com.duendeneon.workshopmongo.dto.UserDTO;
 import com.duendeneon.workshopmongo.repository.UserRepository;
 import com.duendeneon.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -25,5 +26,12 @@ public class UserService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
 	}
 	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+	}
 
 }
